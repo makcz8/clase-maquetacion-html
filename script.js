9a79ebee0f89b0bd03d5c7cb6a26d70629ea3f8c
Javier Rojas <jjrojaspy@gmail.com>
2023-10-26 16:19:01 -0400

    music player template

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
-fetch("https://leonardoapi.onrender.com/music")
+const audioComponent = document.getElementById('audioComponent')
+const mainContainer = document.getElementById('mainContainer')
 
-console.log("chacalito")
+fetch("https://leonardoapi.onrender.com/music")
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (array) {
+        array
+            .map(transFormJSONElementIntoHTMLElement)
+            .forEach(appendElementIntoMainContainer)
+    })
+    .catch(function (reason) {
+        console.error(reason)
+    })
+
+function transFormJSONElementIntoHTMLElement(jsonElement) {
+    const divContainer = document.createElement('div')
+    /* TODO: crear la clase song-style en el style.css */
+    // divContainer.classList.add('song-style')
+    divContainer.innerHTML = /* html */ `            
+        <img class="" src="${jsonElement.path.front}">
+        <p class="">${jsonElement.title}</p>
+        <p class="">${jsonElement.author}</p>
+        <p class="">${jsonElement.album}</p>            
+    `
+    divContainer.addEventListener('click', function () {
+        audioComponent.setAttribute('src', jsonElement.path.audio)
+    })
+
+    return divContainer
+}
+
+function appendElementIntoMainContainer(htmlNode) {
+    mainContainer.appendChild(htmlNode)
+}
